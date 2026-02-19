90555e885507510ee258b236f1e62b50e67c98f5
Ismaïl Lahyane <ismail.lahyane@gmail.com>
2026-02-19 22:58:13 +0100

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,20 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/recordslens---the-future-of-vision/', // ⚠️ mets ici le nom EXACT de ton repo
+
       server: {
         port: 3000,
         host: '0.0.0.0',
       },
+
       plugins: [react()],
+
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
